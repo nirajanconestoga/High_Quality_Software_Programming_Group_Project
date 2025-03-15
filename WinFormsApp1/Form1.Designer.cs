ff65e9ff9cb835e719558aa9ddd65eedb3c274fc
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-03-15 11:12:04 -0400

    Feat: started the group project

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F);
+            label1.Location = new Point(12, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(238, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Total Expenses: $0.00";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F);
+            label2.Location = new Point(12, 121);
+            label2.Name = "label2";
+            label2.Size = new Size(165, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Balance: $0.00";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14F);
+            label3.Location = new Point(12, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(220, 32);
+            label3.TabIndex = 2;
+            label3.Text = "Total Income: $0.00";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(347, 37);
+            button1.Name = "button1";
+            button1.Size = new Size(116, 29);
+            button1.TabIndex = 3;
+            button1.Text = "Add Expense";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(347, 81);
+            button2.Name = "button2";
+            button2.Size = new Size(116, 29);
+            button2.TabIndex = 4;
+            button2.Text = "Add Income";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(347, 126);
+            button3.Name = "button3";
+            button3.Size = new Size(116, 29);
+            button3.TabIndex = 5;
+            button3.Text = "View Expenses";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(535, 554);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
